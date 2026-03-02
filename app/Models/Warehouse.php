5a19d1189d0ed1b45b04a4f8cdefbab81aac6ac9
hawkxc068 <hawkxc068@gmail.com>
2026-03-02 22:07:20 +0700

    feat: update warehouse feature

--- app/Models/Warehouse.php
+++ app/Models/Warehouse.php
@@ -4,8 +4,35 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Warehouse extends Model
 {
     use HasFactory;
+
+    protected $table = 'warehouses';
+
+    protected $fillable = [
+        'uuid',
+        'company_id',
+        'name',
+        'capacity',
+        'description',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->uuid)) {
+                $model->uuid = (string) Str::uuid();
+            }
+        });
+    }
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
 }
