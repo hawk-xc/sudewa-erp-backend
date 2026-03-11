9310150e84a0253098d35b4aea2bdc0da9ba7dd7
hawkxc <hawkxc068@gmail.com>
2026-03-11 09:52:38 +0700

    add: add main company warehouse seeder

---
+++ database/seeders/MainCompanyWarehouseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Company;
+use App\Models\Warehouse;
+use Illuminate\Database\Seeder;
+
+class MainCompanyWarehouseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $main_company = [1, 2, 5];
+
+        $companies = Company::whereIn('id', $main_company)->get();
+
+        foreach ($companies as $company) {
+            Warehouse::create([
+                'company_id' => (int) $company->id,
+                'name' => (string) $company->name.' Warehouse',
+                'capacity' => (int) 100,
+                'description' => null,
+            ]);
+        }
+    }
+}
