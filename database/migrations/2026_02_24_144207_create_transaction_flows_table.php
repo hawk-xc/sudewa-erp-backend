6f1f431bb7391a307edf59274597633ac500a330
hawkxc <hawkxc068@gmail.com>
2026-03-12 14:59:08 +0700

    update: update transaction_proof column

--- database/migrations/2026_02_24_144207_create_transaction_flows_table.php
+++ database/migrations/2026_02_24_144207_create_transaction_flows_table.php
@@ -17,13 +17,15 @@ return new class extends Migration
             $table->foreignId('company_id')->constrained('companies')->cascadeOnDelete();
             $table->foreignId('unit_transaction_id')->nullable()->constrained('unit_transactions')->nullOnDelete();
             $table->date('transaction_date')->nullable(false)->default(date('Y-m-d'));
-            $table->string('description')->nullable(true);
+            $table->text('name')->nullable(true);
+            $table->text('description')->nullable(true);
             $table->decimal('bank_usd_debit', 10, 2)->default(0)->nullable(false);
             $table->decimal('bank_usd_credit', 10, 2)->default(0)->nullable(false);
             $table->decimal('bank_idr_debit', 15, 2)->default(0)->nullable(false);
             $table->decimal('bank_idr_credit', 15, 2)->default(0)->nullable(false);
             $table->decimal('cash_idr_debit', 15, 2)->default(0)->nullable(false);
             $table->decimal('cash_idr_credit', 15, 2)->default(0)->nullable(false);
+            $table->string('transaction_proof')->nullable(true);
             $table->timestamps();
         });
     }
