c78c2762b49db13f46a4a930bbc70f8cd7697b84
hawkxc <hawkxc068@gmail.com>
2026-03-23 21:34:23 +0700

    add: add new schema -> region

---
+++ app/Models/Region.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Region extends Model
+{
+    use HasFactory;
+
+    protected $table = 'regions';
+
+    protected $fillable = [
+        'uuid',
+        'code',
+        'name',
+    ];
+
+    protected static function booted()
+    {
+        static::creating(function ($model) {
+            if (empty($model->uuid)) {
+                $model->uuid = (string) Str::uuid();
+            }
+        });
+    }
+}
