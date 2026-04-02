58ea7769a9b62b864a7d01e4e3422cfcae20003f
hawkxc <hawkxc068@gmail.com>
2026-04-02 08:56:50 +0700

    add: add dummy-fresh script #dangerous in production mode -> don't run it

---
+++ dummy-fresh.sh
@@ -0,0 +1,9 @@
+composer install
+
+php artisan config:clear
+php artisan route:clear
+php artisan cache:clear
+
+php artisan migrate:fresh --seed && php artisan db:seed DummyDataSeeder
+php artisan db:seed MainCompanyWarehouseSeeder
+php artisan db:seed PurchaseUnitTransactionDummySeeder
