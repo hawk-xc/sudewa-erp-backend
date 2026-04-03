bbab18c2daae05470d9f04cf520c4cb32c3a9b04
hawkxc <hawkxc068@gmail.com>
2026-04-03 23:44:16 +0700

    update: update unit type price achive columns

--- app/Models/UnitTypePriceArchive.php
+++ app/Models/UnitTypePriceArchive.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class UnitTypePriceArchive extends Model
 {
     use HasFactory;
+
+    protected $table = 'unit_type_price_archives';
+    
+    protected $fillable = [
+        'uuid',
+        'unit_type_id',
+        'user_id',
+        'buy_price',
+        'sell_price',
+    ];
+
+    protected $casts = [
+        'unit_type_id' => 'integer',
+        'user_id' => 'integer',
+        'buy_price' => 'integer',
+        'sell_price' => 'integer'
+    ];
+
+    public function 
 }
--- database/migrations/2026_04_02_090102_create_unit_type_price_archives_table.php
+++ database/migrations/2026_04_02_090102_create_unit_type_price_archives_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('unit_type_price_archives', function (Blueprint $table) {
             $table->id();
+            $table->uuid();
+            $table->foreignId('unit_type_id')->constrained('users')->nullOnDelete();
+            $table->foreignId('user_id')->constrained('users')->nullOnDelete();
+            $table->decimal('buy_price', 15, 2)->nullable(false)->default(0);
+            $table->decimal('sell_price', 15, 2)->nullable(false)->default(0);
             $table->timestamps();
         });
     }
