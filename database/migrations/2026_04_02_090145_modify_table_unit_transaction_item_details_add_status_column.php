4196672ef36743a2d9a06824782d8cd95ba18578
hawkxc068 <hawkxc068@gmail.com>
2026-04-05 15:46:20 +0700

    merge branch development

--- database/migrations/2026_04_02_090145_modify_table_unit_transaction_item_details_add_status_column.php
+++ database/migrations/2026_04_02_090145_modify_table_unit_transaction_item_details_add_status_column.php
@@ -12,7 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('unit_transaction_item_details', function (Blueprint $table) {
+<<<<<<< HEAD
             $table->enum('status', ['normal', 'minor_damage', 'major_damage', 'returned', 'refunded', 'lost', 'in_repair'])->default('normal')->after('is_forecast');
+=======
+            //
+>>>>>>> 3046f9a (fix: resolve conflict)
         });
     }
 
@@ -22,7 +26,11 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('unit_transaction_item_details', function (Blueprint $table) {
+<<<<<<< HEAD
             $table->dropColumn('status');
+=======
+            //
+>>>>>>> 3046f9a (fix: resolve conflict)
         });
     }
 };
