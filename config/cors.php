0e377d49cdd84629afdef418fb8fd2dbcea7f63e
hawkxc <hawkxc068@gmail.com>
2026-04-20 19:24:32 +0700

    update: add https://dashboard-finances.wajiracorps.co.id domain to cors config

--- config/cors.php
+++ config/cors.php
@@ -23,6 +23,7 @@ return [
     'allowed_origins' => [
         '*',
         'https://wajirapanel.hawk-dev.com',
+        'https://dashboard-finances.wajiracorps.co.id',
         'http://localhost:3000',
     ],
 
