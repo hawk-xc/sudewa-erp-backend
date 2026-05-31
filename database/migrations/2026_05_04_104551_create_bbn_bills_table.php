9f2cab8a9c0711dea10b7474b2ffcf094c77c0ad
hawkxc <hawkxc068@gmail.com>
2026-05-31 23:01:52 +0700

    add: add bbn bills code

--- database/migrations/2026_05_04_104551_create_bbn_bills_table.php
+++ database/migrations/2026_05_04_104551_create_bbn_bills_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('bbn_bills', function (Blueprint $table) {
             $table->id();
             $table->uuid('uuid')->unique();
+            $table->string('code')->unique()->nullable(false);
             $table->foreignId('dealer_id')->constrained('persons');
             $table->date('bill_date');
             $table->date('paid_date')->nullable();
